d9db30510962552a379af9c2dceec6b3938394b7
Perttu Haliseva <perttu@gmail.com>
2020-03-02 20:47:54 +0200

    Add routing to a yet non-existing upload page

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,19 +1,36 @@
 import React from 'react';
+import { BrowserRouter as Router, Link, Switch, Route } from 'react-router-dom';
 import './App.scss';
 import PhotoGallery from './components/PhotoGallery';
+import Upload from './components/Upload';
+
+const bucketName = 'mrc-helsinki-photos';
 
 function App() {
   return (
     <div className="App">
-      <nav id="navigation">
-        <ul>
-          <li>Home</li>
-          <li>Upload</li>
-        </ul>
-      </nav>
-      <div className="App-Contents">
-        <PhotoGallery bucketName="mrc-helsinki-photos" />
-      </div>
+      <Router>
+        <nav id="navigation">
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/upload">Upload</Link>
+            </li>
+          </ul>
+        </nav>
+        <div className="App-Contents">
+          <Switch>
+            <Route exact path="/">
+              <PhotoGallery bucketName={bucketName} />
+            </Route>
+            <Route path="/upload">
+              <Upload></Upload>
+            </Route>
+          </Switch>
+        </div>
+      </Router>
     </div>
   );
 }
